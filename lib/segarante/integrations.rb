727214ec59a60c3935f694ea56636a45d4b83101
Bruno Santos <bruno.santos@segdev.com.br>
2024-05-22 16:33:47 -0300

    Initial COmmit

---
+++ lib/segarante/integrations.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "integrations/version"
+
+module Segarante
+  module Integrations
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
